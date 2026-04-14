68b5ece51d1325206f8203def4f82a10e6f494c5
pradipshiyal21 <pradip.shiyal@mail.nitsan.ai>
2026-04-14 17:25:23 +0530

    Delete testing directory

--- testing/README.md
+++
@@ -1,4 +0,0 @@
-Site Package for the project "testing"
-==============================================================
-
-Add some explanation here.
--- testing/ext_localconf.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-defined('TYPO3') or die('Access denied.');
-
-// Add default RTE configuration
-$GLOBALS['TYPO3_CONF_VARS']['RTE']['Presets']['testing'] = 'EXT:testing/Configuration/RTE/Default.yaml';
