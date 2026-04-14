091ffefa39ab6ce38d5dbbc73a9f7b2453334cdc
Pradip Shiyal <pradip.shiyal@mail.nitsan.ai>
2026-04-14 15:31:07 +0530

    Initial TYPO3 project commit

---
+++ packages/testing/README.md
@@ -0,0 +1,4 @@
+Site Package for the project "testing"
+==============================================================
+
+Add some explanation here.
---
+++ packages/testing/ext_localconf.php
@@ -0,0 +1,6 @@
+<?php
+
+defined('TYPO3') or die('Access denied.');
+
+// Add default RTE configuration
+$GLOBALS['TYPO3_CONF_VARS']['RTE']['Presets']['testing'] = 'EXT:testing/Configuration/RTE/Default.yaml';
